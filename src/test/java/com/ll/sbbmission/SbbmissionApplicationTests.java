ff82b3b49e57e8e5c1b331b5263523eadcd8aeb8
git990412 <wjdwn282@gmail.com>
2023-11-20 13:10:09 +0900

    2_6

--- src/test/java/com/ll/sbbmission/SbbmissionApplicationTests.java
+++ src/test/java/com/ll/sbbmission/SbbmissionApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ll.sbbmission;
 
+import com.ll.sbbmission.question.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
